918ecca78032f413e2243a8ab296497248328826
Ameer Hamza Khetran <hamzaa803@gmail.com>
2023-08-13 03:30:54 +0500

    update:     - cube size changes based on screen width.     - implemented scale.set() on cube.

--- app/components/threeScene1.tsx
+++ app/components/threeScene1.tsx
@@ -1,56 +1,75 @@
 'use client'
 
 import * as THREE from 'three'
-import { Box, } from "@chakra-ui/react"
+import { Box, useBreakpointValue} from "@chakra-ui/react"
 import {useEffect, useRef} from 'react'
 
 export default function Scene() {
 
     const containerRef = useRef<HTMLDivElement | null>(null);
+    const screenWidth = window.innerWidth;
+    const cubeSize = Math.max(0.1, Math.min(2, screenWidth / 1000)); 
 
     useEffect(()=> {
         if(typeof window !== 'undefined') {
-        
+
             const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        const renderer = new THREE.WebGLRenderer();
+            const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+            const renderer = new THREE.WebGLRenderer();
 
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        renderer.setPixelRatio(2);
-        camera.position.z = 5;
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            renderer.setPixelRatio(window.devicePixelRatio);
+            camera.position.z = 5;
 
-        const geometry = new THREE.BoxGeometry();
-        const material = new THREE.MeshBasicMaterial({color: 0x00ffff});
-        const cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
+            const geometry = new THREE.BoxGeometry();
+            const material = new THREE.MeshPhongMaterial({color: 0x00ffff});
+            const cube = new THREE.Mesh(geometry, material);
+            scene.add(cube);
 
-        containerRef.current?.appendChild(renderer.domElement);
-        
-        cube.position.set(0 , 0, 0);
+            const color = 0xFFFFFF;
+            const intensity = 3;
+            const light = new THREE.DirectionalLight(color, intensity);
+            light.position.set(-1, 2, 4);
+            scene.add(light);
 
-        const renderScene = () => {
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
+            containerRef.current?.appendChild(renderer.domElement);
+            
+            cube.position.set(0 , 0, 0);
 
-            renderer.render(scene, camera);
+            //------ Render Scene -------
+            const renderScene = () => {
+                cube.rotation.x += 0.01;
+                cube.rotation.y += 0.01;
 
-            requestAnimationFrame(renderScene);
-        }
+                if(cube.position.x < 2) {
+                    cube.position.x += 0.02
+                }
+                renderer.render(scene, camera);
 
-        const handleResize = () => {
-            const newWidth = window.innerWidth;
-            const newHeight = window.innerHeight;
-            renderer.setSize(newWidth,newHeight);
-            camera.aspect = newWidth / newHeight;
-            camera.updateProjectionMatrix();
-        }
+                requestAnimationFrame(renderScene);
+            }
 
-        window.addEventListener('resize', handleResize);
-        handleResize();
+            //------ Resizing function that will run on window resize --------
+            const handleResize = () => {
+                const newWidth = window.innerWidth;
+                const newHeight = window.innerHeight;
+                camera.aspect = newWidth / newHeight;
+                camera.updateProjectionMatrix();
 
-        renderScene()
+                const newCubeSize = Math.max(0.3, Math.min(1, newWidth / 1000));
+                cube.scale.set(newCubeSize, newCubeSize, newCubeSize);
+                console.log(`new size: ${newCubeSize}`)
+
+                renderer.setSize(newWidth,newHeight);
+
+            }
+
+            window.addEventListener('resize', handleResize);
+            handleResize();
+
+            renderScene()
         }
     },[])
-    return <Box ref={containerRef} w={'100%'} h={'100vh'}/>;
+    return <Box ref={containerRef} w={'100%'} h={'100vh'} m={'0'}/>;
 }
 
