2c7641ddf174fa4c07cd5ef4af20eb5d2fded3a5
Ameer Hamza Khetran <hamzaa803@gmail.com>
2023-08-17 00:06:26 +0500

    gsap installed

--- app/components/threeScene1.tsx
+++ app/components/threeScene1.tsx
@@ -1,8 +1,11 @@
 'use client'
 
 import * as THREE from 'three'
-import { Box, useBreakpointValue} from "@chakra-ui/react"
-import {useEffect, useRef} from 'react'
+import { Box, useBreakpointValue, Container } from "@chakra-ui/react"
+import {useEffect, useRef, useState} from 'react';
+import throttle from 'lodash/throttle';
+import { gsap } from "gsap";
+
 
 export default function Scene() {
 
@@ -13,61 +16,70 @@ export default function Scene() {
 
             const scene = new THREE.Scene();
             const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-            const renderer = new THREE.WebGLRenderer();
+            const renderer = new THREE.WebGLRenderer({ antialias: true});
 
             renderer.setSize(window.innerWidth, window.innerHeight);
             renderer.setPixelRatio(window.devicePixelRatio);
             camera.position.z = 5;
 
             const geometry = new THREE.BoxGeometry();
-            const material = new THREE.MeshPhongMaterial({color: 0x00ffff});
+            const material = new THREE.MeshBasicMaterial({color: 0x00ffff});
             const cube = new THREE.Mesh(geometry, material);
             scene.add(cube);
-
-            const color = 0xFFFFFF;
-            const intensity = 3;
-            const light = new THREE.DirectionalLight(color, intensity);
-            light.position.set(-1, 2, 4);
-            scene.add(light);
 
             containerRef.current?.appendChild(renderer.domElement);
             
             cube.position.set(0 , 0, 0);
 
+            let animationFrameId: number; 
+
+            //------ Stop Render Scene -------
+            const stopAnimation = () => {
+                cancelAnimationFrame(animationFrameId);
+            }
+
+            //------- gsap initial render -----
+     
+      
+            // Use GSAP to animate the cube's movement
+            
+
             //------ Render Scene -------
+            let newCubeSize: number;
+
             const renderScene = () => {
                 cube.rotation.x += 0.01;
                 cube.rotation.y += 0.01;
+                console.log(`Cube Size: ${newCubeSize}`)
 
-                // if(cube.position.x < 2) {
-                //     cube.position.x += 0.02
-                // }
-                renderer.render(scene, camera);
-
-                requestAnimationFrame(renderScene);
+                renderer.render(scene, camera); 
+                animationFrameId = requestAnimationFrame(renderScene);
             }
-
+            
             //------ Resizing function that will run on window resize --------
+            
             const handleResize = () => {
                 const newWidth = window.innerWidth;
                 const newHeight = window.innerHeight;
                 camera.aspect = newWidth / newHeight;
                 camera.updateProjectionMatrix();
-
-                const newCubeSize = Math.max(0.3, Math.min(1, newWidth / 1000));
+                
+                newCubeSize = Math.max(0.3, Math.min(1, newWidth / 1000));
                 cube.scale.set(newCubeSize, newCubeSize, newCubeSize);
-                console.log(`new size: ${newCubeSize}`)
-
+                               
                 renderer.setSize(newWidth,newHeight);
-
             }
+            
 
-            window.addEventListener('resize', handleResize);
+            window.addEventListener('resize', throttle(handleResize, 200));
             handleResize();
-
-            renderScene()
+            animationFrameId = requestAnimationFrame(renderScene);
         }
-    },[])
-    return <Box ref={containerRef} w={'100%'} h={'100vh'} m={'0'}/>;
+    }, [])
+    return (
+        <Container maxWidth={'full'} w={'full'} h={'100vh'} centerContent={true}>
+            <Box ref={containerRef}/>
+        </Container>
+    );
 }
 
